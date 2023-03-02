7bd9fc75061ec5c3b1d121eab60f48a6b7460e81
Eli Fuchsman <efuchsman@gmail.com>
2023-03-02 12:35:41 -0700

    Reseed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,13 @@
 
   tea_1 = Tea.create!(title: "title", description: "desc", temperature: "temp", brew_time: "temp")
 
-  tea_2 = Tea.create!(title: "title", description: "desc", temperature: "temp", brew_time: "temp")
-
   sub_1 = Subscription.create!(title: "title1", price: "$1", frequency: "freq1")
 
   sub_2 = Subscription.create!(title: "title2", price: "$2", frequency: "freq2")
+
+  sub_t_1 = SubscriptionTea.create!(tea_id: tea_1.id, subscription_id: sub_1.id)
+
+  sub_t_2 = SubscriptionTea.create!(tea_id: tea_1.id, subscription_id: sub_2.id)
+
+  sub_t_2 = SubscriptionTea.create!(tea_id: tea_1.id, subscription_id: sub_2.id)
 
