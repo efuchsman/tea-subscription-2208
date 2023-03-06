a50ecd20c4915409b5b32649c9759c4af8988fb9
Eli Fuchsman <efuchsman@gmail.com>
2023-03-06 09:56:51 -0700

    Build controller action

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -14,4 +14,19 @@ class Api::V1::CustomersController < ApplicationController
      render json: { error: "Please provide an id parameter in the URL path" }, status: 400
     end
   end
+
+  def create
+    customer = Customer.new(customer_params)
+    if customer.save
+      render json: { success: "New customer successfully created"}
+    else
+      render json: customer.errors.messages, status: 400
+    end
+  end
+
+  private
+
+  def customer_params
+    params.permit(:first_name, :last_name, :email, :address)
+  end
 end
