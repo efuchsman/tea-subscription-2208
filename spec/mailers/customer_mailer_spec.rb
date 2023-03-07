853112b8545b21b9a3d37784c978b3c32a709b5b
Eli Fuchsman <efuchsman@gmail.com>
2023-03-07 11:39:48 -0700

    Demonstrate mailer in development env

--- spec/mailers/customer_mailer_spec.rb
+++ spec/mailers/customer_mailer_spec.rb
@@ -2,16 +2,20 @@ require "rails_helper"
 
 RSpec.describe CustomerMailer, type: :mailer do
   describe "welcome_customer" do
-    let(:mail) { CustomerMailer.welcome_customer }
+
+    let!(:customer_1) { Customer.create!(first_name: "Eli", last_name: "Fuchsman", email: "el@mail.com", address: "Eli's address")
+    }
+    let!(:mail) { CustomerMailer.with(customer: Customer.last).welcome_customer }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Welcome customer")
-      expect(mail.to).to eq(["to@example.org"])
+
+      expect(mail.subject).to eq("Welcome to Tea Subscription")
+      expect(mail.to).to eq(["el@mail.com"])
       expect(mail.from).to eq(["welcome@teaservice.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.body.encoded).to match("Hello, We are so happy to have you!")
     end
   end
 
