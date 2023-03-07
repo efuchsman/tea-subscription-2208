1fc0de911f24fd06f5ba0aebb4f2373a567fefcf
Eli Fuchsman <efuchsman@gmail.com>
2023-03-07 12:42:43 -0700

    Edit mailer message

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -1,7 +1,7 @@
 class CustomerMailer < ApplicationMailer
   def welcome_customer
     @customer = params[:customer]
-    @greeting = "Hello"
+    @greeting = "Hello #{@customer.first_name}"
 
     mail(to: @customer.email, subject: "Welcome to Tea Subscription")
   end
