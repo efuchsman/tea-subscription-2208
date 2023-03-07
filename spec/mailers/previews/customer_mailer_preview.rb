ff6a1d6c30866fcae727e7457fd9893cc48bef23
Eli Fuchsman <efuchsman@gmail.com>
2023-03-06 19:01:17 -0700

    WIP mailer

--- spec/mailers/previews/customer_mailer_preview.rb
+++ spec/mailers/previews/customer_mailer_preview.rb
@@ -1,9 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/customer_mailer
 class CustomerMailerPreview < ActionMailer::Preview
-
   # Preview this email at http://localhost:3000/rails/mailers/customer_mailer/welcome_customer
   def welcome_customer
     CustomerMailer.with(customer: Customer.last).welcome_customer
   end
-
 end
